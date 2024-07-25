fc66a3529b8b462c51a8bad00f6bd6ec516f2e0e
AlessandroVF <147666927+AlessandroVF@users.noreply.github.com>
2024-07-24 20:34:48 -0500

    Delete admin directory
    1

--- admin/config/controladores/categorias/listado_categoria.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$sql_categorias = "SELECT * FROM categoria WHERE activo_categoria=1";
-$query_categorias = $pdo->prepare($sql_categorias);
-$query_categorias->execute();
-$categorias_datos = $query_categorias->fetchAll(PDO::FETCH_ASSOC);
-
-
-?>
--- admin/config/controladores/empleados/listado_empleado.php
+++
@@ -1,8 +0,0 @@
-<?php
-$sql_empleados = "SELECT * FROM empleado ";
-$query_empleados = $pdo->prepare($sql_empleados);
-$query_empleados->execute();
-$empleados_datos = $query_empleados->fetchAll(PDO::FETCH_ASSOC);
-
-
-?>
